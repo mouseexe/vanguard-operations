e6c152859c1e655210d0ccc2fa93cccff1cf2371
Kay Schwenk <kay.s.schwenk@gmail.com>
2022-06-28 00:30:39 -0400

    Fixed it again

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ async def on_message(message):
 
         then = datetime.now()
         if time != '' and time != 'override':
-            then = date.fromisoformat(time)
+            then = datetime.fromisoformat(time)
         now = datetime.now()
         hour = int(datetime.now().strftime('%H'))
         day = datetime.now().weekday()
