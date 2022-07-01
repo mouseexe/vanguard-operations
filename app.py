eeca103899c4ed337b4f1517703fc0b9b5e9e09d
Kay Schwenk <kay.s.schwenk@gmail.com>
2022-06-30 22:43:34 -0400

    Cooldown and scheduling

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import discord
 import os
 import math
+import re
 from datetime import datetime
 
 # Token is a secret stored on okd
@@ -8,7 +9,8 @@ TOKEN = os.environ.get('DISCORDTOKEN', 'default value')
 
 client = discord.Client()
 
-cadence = 30
+global_cadence = 10
+user_cadence = 120
 
 
 # that was then, this is now
@@ -17,8 +19,8 @@ def get_time_elapsed(that, this):
     return math.floor(delta.total_seconds() / 60)
 
 
-def write(message):
-    timestamp = open('timestamp', 'w')
+def write(file, message):
+    timestamp = open(file, 'w')
     timestamp.write(message)
     timestamp.close()
 
@@ -31,7 +33,7 @@ async def on_message(message):
 
     # reset clock if admin wants to
     if message.content == '/reset' and message.author.guild_permissions.administrator:
-        write('override')
+        write('timestamp', 'override')
         await message.add_reaction('✔')
 
     # This is the ID for the Vanguard Operations bot
@@ -41,40 +43,59 @@ async def on_message(message):
         time = timestamp.read()
         timestamp.close()
 
+        # read timestamp file for the last user ping
+        userstamp = open(message.author, 'r')
+        usertime = userstamp.read()
+        userstamp.close()
+
         then = datetime.now()
         if time != '' and time != 'override':
             then = datetime.fromisoformat(str(time))
-        now = datetime.now()
-        hour = int(datetime.now().strftime('%H'))
-        day = datetime.now().weekday()
+
+        userthen = datetime.now()
+        if usertime != '' and time != 'override':
+            userthen = datetime.fromisoformat(str(usertime))
+
+        if '<t:' in message.content:
+            now = datetime.fromtimestamp(int(re.search('<t:.{10}>', message.content).group(0)[3:13]))
+        else:
+            now = datetime.now()
+        hour = int(now.strftime('%H'))
+        day = now.weekday()
         msg = ''
 
-        if time == '' or time == 'override' or get_time_elapsed(then, now) >= cadence:
+        if time == '' or time == 'override' or (get_time_elapsed(then, now) >= global_cadence and get_time_elapsed(userthen, now) >= user_cadence):
             # write here, write now
-            write(str(now))
+            write('timestamp', str(now))
+            write(message.author, str(now))
             if (day == 4 and hour >= 21) or 4 < day <= 6 or (day == 0 and hour <= 2):
                 # This is the ID for Weekend
-                msg = '<@&991109494253822012>'
+                # msg = '<@&991109494253822012>'
+                msg = 'weekend'
             # times seem to be four hours ahead
             # 2 AM to 8 AM
             if 6 <= hour < 12:
                 # This is the ID for Morning (2 AM - 8 AM)
-                msg = '<@&991090248433950803>'
+                # msg = '<@&991090248433950803>'
+                msg = 'morning'
             # 8 AM to 5 PM
             if 12 <= hour < 21:
                 # This is the ID for Day (8 AM - 5 PM)
-                msg = '<@&991090325894336542>'
+                # msg = '<@&991090325894336542>'
+                msg = 'day'
             # 5 PM to 10 PM
             if 21 <= hour < 24 or 0 <= hour < 2:
                 # This is the ID for Evening (5 PM - 10 PM)
-                msg = '<@&991090361369784360>'
+                # msg = '<@&991090361369784360>'
+                msg = 'evening'
             # 10 PM to 2 AM
             if 2 <= hour < 6:
                 # This is the ID for Night (10 PM - 2 AM)
-                msg = '<@&991090429342679110>'
+                # msg = '<@&991090429342679110>'
+                msg = 'night'
             await message.reply(msg)
         else:
-            await message.reply('Ping available in ' + str(cadence - get_time_elapsed(then, now)) + ' minutes.')
+            await message.reply('Ping available in ' + str(max((global_cadence - get_time_elapsed(then, now)), (user_cadence - get_time_elapsed(userthen, now)))) + ' minutes.')
     # else:
         # print(str(message.author) + ': ' + message.content)
 
