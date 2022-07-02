229a9e996f3fd5d2b7d674d389ea226561f4d568
Kay Schwenk <kay.s.schwenk@gmail.com>
2022-07-02 10:30:25 -0400

    Weekend fix

--- app.py
+++ app.py
@@ -87,31 +87,35 @@ async def on_message(message):
             # write here, write now
             write('timestamp', str(now))
             write(str(message.author), str(now))
+
+            # times seem to be four hours ahead because of UTC
+            # Monday = 0, Sunday = 6
+            # Check if weekend before setting any other pings
             if (day == 4 and hour >= 21) or 4 < day <= 6 or (day == 0 and hour <= 2):
                 # This is the ID for Weekend
                 # msg = '<@&991109494253822012>'
                 msg = 'weekend'
-            # times seem to be four hours ahead
-            # 2 AM to 8 AM
-            if 6 <= hour < 12:
-                # This is the ID for Morning (2 AM - 8 AM)
-                # msg = '<@&991090248433950803>'
-                msg = 'morning'
-            # 8 AM to 5 PM
-            if 12 <= hour < 21:
-                # This is the ID for Day (8 AM - 5 PM)
-                # msg = '<@&991090325894336542>'
-                msg = 'day'
-            # 5 PM to 10 PM
-            if 21 <= hour < 24 or 0 <= hour < 2:
-                # This is the ID for Evening (5 PM - 10 PM)
-                # msg = '<@&991090361369784360>'
-                msg = 'evening'
-            # 10 PM to 2 AM
-            if 2 <= hour < 6:
-                # This is the ID for Night (10 PM - 2 AM)
-                # msg = '<@&991090429342679110>'
-                msg = 'night'
+            else:
+                # 2 AM to 8 AM
+                if 6 <= hour < 12:
+                    # This is the ID for Morning (2 AM - 8 AM)
+                    # msg = '<@&991090248433950803>'
+                    msg = 'morning'
+                # 8 AM to 5 PM
+                if 12 <= hour < 21:
+                    # This is the ID for Day (8 AM - 5 PM)
+                    # msg = '<@&991090325894336542>'
+                    msg = 'day'
+                # 5 PM to 10 PM
+                if 21 <= hour < 24 or 0 <= hour < 2:
+                    # This is the ID for Evening (5 PM - 10 PM)
+                    # msg = '<@&991090361369784360>'
+                    msg = 'evening'
+                # 10 PM to 2 AM
+                if 2 <= hour < 6:
+                    # This is the ID for Night (10 PM - 2 AM)
+                    # msg = '<@&991090429342679110>'
+                    msg = 'night'
             await message.reply(msg)
         else:
             await message.reply('Ping available in ' + str(max((global_cadence - get_time_elapsed(then, now)), (user_cadence - get_time_elapsed(userthen, now)))) + ' minutes.')
