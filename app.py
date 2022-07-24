a7c3e710c420d616dac804ee8a01afa1ab591679
Kay Schwenk <kay.s.schwenk@gmail.com>
2022-07-24 18:25:54 -0400

    Refactoring and timeslot specific cooldowns w/ logging

--- app.py
+++ app.py
@@ -39,6 +39,31 @@ def is_liftable(reactions):
     return True
 
 
+def get_timeslot(time):
+    hour = int(time.strftime('%H'))
+    day = time.weekday()
+
+    # times seem to be four hours ahead because of UTC
+    # Monday = 0, Sunday = 6
+    # Bot will send back the original message, with the bot ping replaced with the relevant timezone ping
+    # Check if weekend before setting any other pings
+    if (day == 4 and hour >= 21) or 4 < day <= 6 or (day == 0 and hour <= 2):
+        return 'weekend'
+    else:
+        # 2 AM to 8 AM
+        if 6 <= hour < 12:
+            return 'morning'
+        # 8 AM to 5 PM
+        if 12 <= hour < 21:
+            return 'day'
+        # 5 PM to 10 PM
+        if 21 <= hour < 24 or 0 <= hour < 2:
+            return 'evening'
+        # 10 PM to 2 AM
+        if 2 <= hour < 6:
+            return 'night'
+
+
 @client.event
 async def on_message(message):
     # we do not want the bot to reply to itself
@@ -50,8 +75,19 @@ async def on_message(message):
 
     # reset clock if admin wants to
     if message.content == '/reset' and message.author.guild_permissions.administrator:
-        write('timestamp', 'override')
+        write('weekend', 'override')
+        write('morning', 'override')
+        write('day', 'override')
+        write('evening', 'override')
+        write('night', 'override')
         await message.add_reaction('✔')
+
+    if '/read' in message.content and message.author.guild_permissions.administrator:
+        readfile = message.content[5:message.content.len]
+        timefile = open(readfile, 'r')
+        read = timefile.read()
+        timefile.close()
+        await message.reply(read)
 
     # vote lift reaction start (only works if message contains /votelift and a user to lift
     if '/votelift' in message.content and bool(re.search('<@.{18}>', message.content)):
@@ -59,14 +95,23 @@ async def on_message(message):
 
     # This is the ID for the Vanguard Operations bot
     if '<@991063755939016875>' in message.content:
+        now = datetime.now()
+        # If message contains a timestamp, use that instead of the current time
+        if '<t:' in message.content:
+            time = datetime.fromtimestamp(int(re.search('<t:.{10}>', message.content).group(0)[3:13]))
+        else:
+            # Otherwise just use the current time
+            time = now
+        timeslot = get_timeslot(time)
+
         try:
             # Read timestamp file for last ping time
-            timestamp = open('timestamp', 'r')
+            timestamp = open(timeslot, 'r')
             time = timestamp.read()
             timestamp.close()
         except:
             # Create file and populate if it doesn't exist
-            write('timestamp', '')
+            write(timeslot, '')
             time = ''
 
         try:
@@ -89,50 +134,33 @@ async def on_message(message):
         if usertime != '':
             userthen = datetime.fromisoformat(str(usertime))
 
-        # If message contains a timestamp, use that instead of the current time
-        if '<t:' in message.content:
-            now = datetime.fromtimestamp(int(re.search('<t:.{10}>', message.content).group(0)[3:13]))
-        else:
-            # Otherwise just use the current time
-            now = datetime.now()
-        hour = int(now.strftime('%H'))
-        day = now.weekday()
-        msg = ''
-
         if time == '' or time == 'override' or (get_time_elapsed(then, now) >= global_cadence and (usertime == '' or get_time_elapsed(userthen, now) >= user_cadence)):
             # write here, write now
-            write('timestamp', str(now))
+            write(timeslot, str(now))
             write(str(message.author), str(now))
 
-            # times seem to be four hours ahead because of UTC
-            # Monday = 0, Sunday = 6
-            # Bot will send back the original message, with the bot ping replaced with the relevant timezone ping
-            # Check if weekend before setting any other pings
-            if (day == 4 and hour >= 21) or 4 < day <= 6 or (day == 0 and hour <= 2):
+            msg = ''
+            if timeslot == 'weekend':
                 # This is the ID for Weekend
                 msg = message.content.replace('<@991063755939016875>', '<@&991109494253822012>')
-                # msg = 'weekend'
             else:
                 # 2 AM to 8 AM
-                if 6 <= hour < 12:
+                if timeslot == 'morning':
                     # This is the ID for Morning (2 AM - 8 AM)
                     msg = message.content.replace('<@991063755939016875>', '<@&991090248433950803>')
-                    # msg = 'morning'
                 # 8 AM to 5 PM
-                if 12 <= hour < 21:
+                if timeslot == 'day':
                     # This is the ID for Day (8 AM - 5 PM)
                     msg = message.content.replace('<@991063755939016875>', '<@&991090325894336542>')
-                    # msg = 'day'
                 # 5 PM to 10 PM
-                if 21 <= hour < 24 or 0 <= hour < 2:
+                if timeslot == 'evening':
                     # This is the ID for Evening (5 PM - 10 PM)
                     msg = message.content.replace('<@991063755939016875>', '<@&991090361369784360>')
-                    # msg = 'evening'
                 # 10 PM to 2 AM
-                if 2 <= hour < 6:
+                if timeslot == 'night':
                     # This is the ID for Night (10 PM - 2 AM)
                     msg = message.content.replace('<@991063755939016875>', '<@&991090429342679110>')
-                    # msg = 'night'
+            msg = timeslot
             await message.reply(msg)
         else:
             await message.reply('Ping available in ' + str(max((global_cadence - get_time_elapsed(then, now)), (user_cadence - get_time_elapsed(userthen, now)))) + ' minutes.')
@@ -143,28 +171,30 @@ async def on_message(message):
 
 @client.event
 async def on_reaction_add(reaction, user):
-    # you only have X minutes to get the votes
-    if '/votelift' in reaction.message.content and get_time_elapsed(reaction.message.created_at, datetime.now()) > vote_expiration:
-        # clear votes
-        await reaction.message.clear_reaction('🗳️')
-        # show vote failed emoji
-        await reaction.message.add_reaction('❌')
-        return
+    if '/votelift' in reaction.message.content and is_liftable(reaction.message.reactions):
+        # you only have X minutes to get the votes
+        if reaction.emoji == '🗳️':
+            if get_time_elapsed(reaction.message.created_at, datetime.now()) > vote_expiration:
+                # clear votes
+                await reaction.message.clear_reaction('🗳️')
+                # show vote failed emoji
+                await reaction.message.add_reaction('❌')
+                return
 
-    # If the reaction is on a votelift message, and it hits X votes total (bot reaction included), lift and clear reactions
-    if '/votelift' in reaction.message.content and reaction.emoji == '🗳️' and reaction.count >= votes and is_liftable(reaction.message.reactions):
-        lifted_id = int(re.search('<@.{18}>', reaction.message.content).group(0)[2:20])
-        lifted = await reaction.message.guild.fetch_member(lifted_id)
-        afk_channel = client.get_channel(878743239199424532)
-        await lifted.move_to(afk_channel)
-        # remove all votes
-        await reaction.message.clear_reaction('🗳️')
-        # add angel to signify lifting has occurred
-        await reaction.message.add_reaction('👼')
+            # If the reaction is on a votelift message, and it hits X votes total (bot reaction included), lift and clear reactions
+            if reaction.count >= votes:
+                lifted_id = int(re.search('<@.{18}>', reaction.message.content).group(0)[2:20])
+                lifted = await reaction.message.guild.fetch_member(lifted_id)
+                afk_channel = client.get_channel(878743239199424532)
+                await lifted.move_to(afk_channel)
+                # remove all votes
+                await reaction.message.clear_reaction('🗳️')
+                # add angel to signify lifting has occurred
+                await reaction.message.add_reaction('👼')
 
-    # don't allow people to react with angel before lifting occurs
-    if '/votelift' in reaction.message.content and is_liftable(reaction.message.reactions) and reaction.emoji == '👼' and user != client.user:
-        await reaction.remove(user)
+        # don't allow people to react with angel before lifting occurs
+        if reaction.emoji == '👼' and user != client.user:
+            await reaction.remove(user)
 
 
 @client.event
